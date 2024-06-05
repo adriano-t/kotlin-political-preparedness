33b20ed1d702f2aa2e68c41284f483f99402222a
Adriano Tumminelli <a.tumminelli@reply.it>
2024-06-05 10:55:54 +0200

    feat: add Timber

--- app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.android.politicalpreparedness
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
         setContentView(R.layout.activity_main)
     }
 }
